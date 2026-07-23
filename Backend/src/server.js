5c1c7b6dd33561df29979c7efcdac73779dd98af
Om Urkude <omurkude274@gmail.com>
2026-07-23 20:45:02 +0530

    chore: initialize backend with Express and MongoDB setup

---
+++ Backend/src/server.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv");
+
+// Load environment variables
+dotenv.config();
+
+const app = require("./app");
+const connectDB = require("./config/db");
+
+const PORT = process.env.PORT || 5000;
+
+// Connect to Database
+connectDB();
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
+
+
