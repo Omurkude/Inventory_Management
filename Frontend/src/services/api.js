bc79e42424c110a8c4f99109d4768928ccdb3c0d
Omurkude <145600551+Omurkude@users.noreply.github.com>
2026-07-25 20:21:51 +0530

    Change base URL in API configuration

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://inventory-management-g86j.onrender.com",
+  baseURL: "heheh",
 });
 
 api.interceptors.request.use((config) => {
